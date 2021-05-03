fd9b603251a91b25c650c385cf1e8497904d4f3b
moonstrider20 <46873254+moonstrider20@users.noreply.github.com>
2021-05-03 12:44:54 -0400

    some changes

---
+++ FrogMechanics/Assets/Scripts/ChangeTextFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextFile : MonoBehaviour
+{
+    public ActivateTextAtLine activateText;
+    public bool duck;
+    public bool rat;
+    public bool ax;
+
+    string duckPath = "Assets/Talking.txt";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (duck)
+            if (PlayerController.strawberryArt)
+            {
+                activateText.theText = Resources.Load(duckPath) as TextAsset;
+            }
+    }
+}
